d2c566ed03dbe4ba8b8c1955591236194b05800c
Douglas Silva <dogametal@hotmail.com>
2020-08-03 14:36:06 -0300

    Bad Solution_2

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,19 +38,13 @@ public class Program {
 			checkIn = sdf.parse(sc.next());
 			System.out.print("Check-out date(mm/DD/yyyy) :");
 			checkOut = sdf.parse(sc.next());
-			
-			Date now = new Date();
-			
-			if(checkIn.before(now) || checkOut.before(now)) {
-				
-				System.out.println("Error in recervation : Reservation dates for update must be furture dates .");
+ 
+			String error = reservation.updateDates(checkIn, checkOut);
+			if (error !=null) {
+				System.out.println("Error in reservation "+ error);
 			}
-			else if (!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation : Check-out date must be after check-in date.");	
-			}
-			else {			
-			reservation.updateDates(checkIn, checkOut);
-			System.out.println("Reservation : " + reservation);
+			else {
+				System.out.println("Reservation : " + reservation);
 			}
 		}
 		
